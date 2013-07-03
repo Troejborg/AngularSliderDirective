42a1056c8df6cc86ea963e7d0feeefa88f3a1943
Ronnie Trøjborg <troejborg87@gmail.com>
2013-07-03 12:50:27 +0200

    Proper binding of test directive

--- app/js/directives.js
+++ app/js/directives.js
@@ -2,10 +2,36 @@
 
 /* Directives */
 
+// add a namespace for custom directives
+angular.module('myApp.directives', []);
 
-angular.module('myApp.directives', [])
-    .directive('blink', function() {
-        return {
-            template: '<marquee scrollamount="100%">Blink!</marquee>'
-        };
-    });
+angular.module('myApp.directives').directive('blink', function() {
+    return {
+        restrict: 'E',
+        transclude: true,
+        templateUrl: 'partials/blink.html',
+        scope: {},
+        link: function(scope, element, attrs)
+        {
+            var marquee = element.find("marquee");
+            var input = element.find("input");
+
+            // Attach blur event
+            input.bind('blur', function()
+            {
+                scope.$apply(function()
+                {
+                    scope.editMode = false;
+                    marquee.text(input.val());
+                });
+            });
+            // edit mode boolean controls the visibility of the blink and input
+            scope.editMode = false;
+            // called when the marquee tag is clicked
+            scope.edit = function() {
+                scope.editMode = true;
+                input.val(marquee.text());
+            };
+        }
+    };
+});
